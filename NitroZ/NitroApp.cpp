2d38c86355b7d0900fa47162ce6e0ffdc69d1bf3
Wilfred0zz <Wilfredo.Recinos52@myhunter.cuny.edu>
2021-10-22 13:01:10 -0400

    Used CMake to use glfw and now it can only run when win32 is ticked after configuration before generating

--- NitroZ/NitroApp.cpp
+++ NitroZ/NitroApp.cpp
@@ -2,15 +2,25 @@
 
 #include "NitroApp.h"
 
+#include "GLFW/glfw3.h"
 
 namespace NitroZ
 {
 	void NitroApp::Run()
 	{
 		std::cout << "NitroZ is running..." << std::endl;
+
+		GLFWwindow* window;
+
+		glfwInit();
+		window = glfwCreateWindow(800, 600, "Test", NULL, NULL);
+
 		while (true) 
 		{
 			OnUpdate();
+
+			glfwSwapBuffers(window);
+			glfwPollEvents();
 		}
 	}
 
